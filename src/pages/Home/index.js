efe03ef8cb13860eb3dcc2dbc455c3a519b3430e
“Githubxxx17” <s486275391@outlook.com>
2023-11-29 11:28:04 +0800

    feat:echart封装

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,24 @@
+import { BarChart } from "./components/BarChart";
+import "./index.scss";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="barChart">
+      <BarChart
+        title={"三大框架满意度"}
+        xData={["Vue", "React", "Angular"]}
+        sData={[2000, 5000, 1000]}
+        style={{ width: "500px", height: "400px" }}
+      />
+
+      <BarChart
+        title={"三大框架使用度"}
+        xData={["Vue", "React", "Angular"]}
+        sData={[200, 500, 100]}
+        style={{ width: "500px", height: "400px" }}
+      />
+    </div>
+  );
 };
+
 export default Home;
